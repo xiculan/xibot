fb18d7dd79c5fb716f0a208f2d8c9d8b9cb3a7c6
root <root@xibot-r194.lan>
2020-03-15 16:45:32 +0000

    canvis_web_xibot

---
+++ web_xibot/actualitza_web.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+sudo rm -R /var/www/html/*
+sudo cp -r * /var/www/html/
+
+
+
+#message=$1
+
+
+#if [ -n "$1" ]
+#then
+#  git add .
+#  git commit -m "$message"
+#   echo $message
+#else
+#  echo "falta mensaje"
+#  exit
+#fi
+
+#git push
+
+
+
+#exit
+
+
+
